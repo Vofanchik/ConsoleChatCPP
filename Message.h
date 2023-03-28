9dbc4717be0682f0f2a64db9ea59c0bee123282e
MirkoUser <mirko513@yandex.ru>
2023-03-28 12:19:48 +0300

    ~Message() {};

--- Message.h
+++ Message.h
@@ -1,12 +1,20 @@
 ﻿#pragma once
+#include "User.h"
 
 class Message
 {
 public:
-	Message() {};
-	~Message() {};
+	Message(const string& from, const string& to, const string& text)
+		:_from(from), _to(to), _text(text) {}
+	const string& GetFrom()const{ return _from;}
+	const string& GetTo()const { return _to;}
+	const string& GetText()const { return _text;}
+
+	//~Message() {};
 
 private:
-
+	const string _from;
+	const string _to;
+	const string _text;
 };
 
